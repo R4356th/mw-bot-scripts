8ddf1cb3c869005fb39e4f99d95e465bd69a6ec9
Gitea <radman.siddiki@gmail.com>
2021-01-20 16:45:24 +0600

    Add catreplace.py

---
+++ catreplace.py
@@ -0,0 +1,21 @@
+# Import functions and configuration (from configuration.py).
+from mw_api_client import Wiki
+from configuration import WIKI_API_URL, WIKI_PASSWORD, WIKI_USERNAME, WIKI_USERAGENT
+
+# Log in.
+wiki = Wiki(WIKI_API_URL, WIKI_USERAGENT)
+wiki.login(WIKI_USERNAME, WIKI_PASSWORD)
+print('Logged in.')
+
+# Take user input.
+cat = input('Category name (without "Category:")')
+strtoreplace = input('Text to replace: ')
+strtoreplacewith = input('Text to replace with: ')
+summ = input('Summary: ')
+
+# Edit pages.
+for page in wiki.category(cat).categorymembers():
+    print(page.title)
+    page.replace(strtoreplace, strtoreplacewith, summ)
+
+print('Process completed.')
